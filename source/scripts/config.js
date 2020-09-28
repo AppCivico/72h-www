2e94d63d0fc8e3960c462ccc898eec9e14f64002
eduruiz <contato@eduruiz.com.br>
2020-09-28 15:42:16 -0300

    Update productionDomains validation xD

--- source/scripts/config.js
+++ source/scripts/config.js
@@ -2,8 +2,8 @@ const productionDomains = ['72horas.org', 'quirky-lamport-b80cd2.netlify.app'];
 
 export default {
   api: {
-    domain: (window.location.hostname.indexOf(productionDomains) === -1
-      ? 'https://dev-h72-api.appcivico.com/v1/'
-      : 'https://h72-api.appcivico.com/v1/'),
+    domain: (productionDomains.indexOf(window.location.hostname) > -1
+      ? 'https://h72-api.appcivico.com/v1/'
+      : 'https://dev-h72-api.appcivico.com/v1/'),
   },
 };
