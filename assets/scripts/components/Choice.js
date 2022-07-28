8cc0c54ae684459d08f47ac8077f746b10e376d2
sobral <robson@oficiodesign.com>
2022-07-28 13:49:18 -0300

    Add list box component

---
+++ assets/scripts/components/Choice.js
@@ -0,0 +1,79 @@
+export default {
+  name: 'Choice',
+  template: "#choice-markup",
+  inheritAttrs: false,
+  props: {
+    options: {
+      type: Array,
+      default: () => [],
+    },
+    name: {
+      type: String,
+      default: '',
+    },
+    labelForEmpty: {
+      type: String,
+      default: '',
+    },
+    multiple: {
+      type: Boolean,
+      default: false,
+    },
+    required: {
+      type: Boolean,
+      default: false,
+    },
+    type: {
+      type: String,
+      default: 'multiple_choice',
+    },
+    value: {
+      type: [Array, Number, String],
+      default: () => [],
+      validator: (value) => !(!Array.isArray(value) && typeof value === 'object'),
+    },
+  },
+  computed: {
+    currentValues({ value } = this) {
+      if (Array.isArray(value)) {
+        return value.map((x) => (x ? String(x) : x));
+      } else if (typeof value === 'object') {
+        return value.map((x) => (x ? String(x) : x));
+      }
+      return [(value ? String(value) : value)];
+    },
+    normalizedOptions({ options } = this) {
+      return options.map((x) => { return typeof x === 'object' ? {...x, id: x.id || x.value, label: x.label || x.acronym || x.name, value: x.value || x.id } : x});
+    },
+    normalizedType({ multiple, options } = this) {
+      if (!multiple) {
+        if (options.length <= 6) {
+          return 'radio';
+        }
+        return 'select';
+      }
+      return 'checkbox';
+    },
+    lastSelected({ currentValues } = this) {
+      return currentValues.length
+        ? currentValues[currentValues.length - 1]
+        : '';
+    }
+  },
+  methods: {
+    emit(e) {
+      const { value } = e.target;
+      const { multiple = false, currentValues = [] } = this;
+
+      if (multiple) {
+        const newValues = e.target.checked
+          ? currentValues.concat([value])
+          // eslint-disable-next-line eqeqeq
+          : currentValues.filter((x) => x != value);
+        this.$emit('input', newValues);
+      } else {
+        this.$emit('input', value);
+      }
+    },
+  },
+};
