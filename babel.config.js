b1aa475453e2dd43101fdf2f5194cf93a75c6bd6
Sobral <servers@eokoe.com>
2022-08-11 17:15:14 -0300

    Update babel settings

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,14 @@
 module.exports = {
-    "presets": ["@babel/preset-env"],
-    "plugins": [
-        ["@babel/transform-runtime"]
+  "presets": [
+    [
+      "@babel/preset-env",
+      {
+        useBuiltIns: "usage",
+        corejs: "3.24.1"
+      }
     ]
+  ],
+  "plugins": [
+      ["@babel/transform-runtime"]
+  ]
 }
