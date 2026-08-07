b01ae4c4a279775e50bb5225ef8a0535675384e8
Sobral <servers@eokoe.com>
2026-08-07 15:35:34 -0300

    feat: Update API origin

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -1,9 +1,3 @@
-const productionDomains = [
-  // '72horas.org',
-  // 'quirky-lamport-b80cd2.netlify.app',
-  // '2020.72horas.org',
-];
-
 export default {
   run: {
     2020: '2030402020',
@@ -11,8 +5,6 @@ export default {
     2024: '2045202024',
   },
   api: {
-    domain: (productionDomains.indexOf(window.location.hostname) > -1
-      ? 'https://h72-api.appcivico.com/v1/'
-      : 'https://dev-h72-api.appcivico.com/v1/'),
+    domain: 'https://h72-api.appcivico.com/v1/',
   },
 };
